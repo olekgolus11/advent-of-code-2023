e301be8c02fc3f9b5edd675d6bd9b2b16e215eb5
Olek Golus <olekgolus02@gmail.com>
2023-12-16 21:35:46 +0100

    Day14 ex1

---
+++ src/day14-1.ts
@@ -0,0 +1,55 @@
+import FileService from "./utils/FileService";
+
+enum Tile {
+    ROUNDED_ROCK = "O",
+    CUBE_ROCK = "#",
+    EMPTY_SPACE = ".",
+}
+
+const fileService = new FileService();
+
+const field = fileService.getFileContentCells("input14.txt");
+
+const isEmptySpaceAbove = (rowIndex: number, colIndex: number) => {
+    if (rowIndex === 0) return false;
+    return field[rowIndex - 1][colIndex] === Tile.EMPTY_SPACE;
+};
+
+const slideRockUp = (rowIndex: number, colIndex: number) => {
+    if (rowIndex === 0) return;
+    field[rowIndex - 1][colIndex] = Tile.ROUNDED_ROCK;
+    field[rowIndex][colIndex] = Tile.EMPTY_SPACE;
+};
+
+const slideAllRocksOnce = () => {
+    field.forEach((row, rowIndex) => {
+        if (rowIndex === 0) return; // skip first row
+
+        row.forEach((tile, colIndex) => {
+            const thisTile = tile as Tile;
+
+            if (thisTile !== Tile.ROUNDED_ROCK) return;
+
+            if (isEmptySpaceAbove(rowIndex, colIndex)) {
+                slideRockUp(rowIndex, colIndex);
+            }
+        });
+    });
+};
+
+while (true) {
+    const fieldBefore = field.map((row) => [...row]);
+    slideAllRocksOnce();
+    if (fieldBefore.join("") === field.join("")) break;
+}
+
+let answer = 0;
+const numberOfRows = field.length;
+
+field.forEach((row, index) => {
+    const rowValue = numberOfRows - index;
+    const numberOfRoundedRocks = row.filter((tile) => tile === Tile.ROUNDED_ROCK).length;
+    answer += rowValue * numberOfRoundedRocks;
+});
+
+console.log(answer);
